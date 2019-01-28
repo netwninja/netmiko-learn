e7b47c5ac981375e55fdaa1df04b62ea8da2b98d
Gaurav Sood <gaurav.sud@hotmail.com>
2019-01-27 17:33:27 -0800

    creating sol3.py

---
+++ netmiko-scripts/sol3.py
@@ -0,0 +1,26 @@
+from netmiko import ConnectHandler 
+from getpass import getpass 
+
+pasw = getpass()
+
+R1 = {
+  "device_type": "cisco_xe",
+  "host": "cisco3.lasthop.io",
+  "username": "pyclass",
+  "password": pasw,
+}
+
+R2 = {
+  "device_type": "cisco_xe",
+  "host": "cisco4.lasthop.io",
+  "username": "pyclass",
+  "password": pasw,
+}
+
+for device in (R1, R2):
+    net_connect = ConnectHandler(**device)
+    output = net_connect.send_command("show version")
+    
+with open("show_version.txt", "w") as save_data:
+    save_data.write(output)
+
