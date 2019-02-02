28783ee7e0b53c5d490a1b423d218604bf7c3d13
Gaurav Sood <gaurav.sud@hotmail.com>
2019-02-01 16:38:04 -0800

    Adding new netmiko python script

---
+++ netmiko-scripts/del-file.py
@@ -0,0 +1,30 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+pasw = getpass()
+
+node1 = {
+  "device_type": "cisco_xe",
+  "host": "cisco3.lasthop.io",
+  "username": "pyclass",
+  "password": pasw,
+  "session_log": "node1.txt",
+}
+
+net_connect = ConnectHandler(**node1)
+print(net_connect.find_prompt())
+
+command = 'delete flash:/del.txt'
+command2 = 'dir flash:'
+
+output = net_connect.send_command_timing(command2,
+    strip_prompt=False, strip_command=False)
+output += net_connect.send_command_timing(command,
+    strip_prompt=False, strip_command=False)
+output += net_connect.send_command_timing('',
+    strip_prompt=False, strip_command=False)
+output += net_connect.send_command_timing("y",
+    strip_prompt=False, strip_command=False)
+output += net_connect.send_command_timing(command2,
+    strip_prompt=False, strip_command=False)
+print (output)
